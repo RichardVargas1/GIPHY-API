bbb4020bea37ea6a16612e8a95ee121cba0be697
RichardVargas1 <richard.vargas95@gmail.com>
2018-09-23 23:30:48 -0500

    Final.

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -7,7 +7,8 @@ $('#superherosView').empty();
     var superhero = $(this).attr('data-name');
 
 var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + superhero + "&api_key=dc6zaTOxFJmzC&limit=10";
-    $.ajax({url: queryURL, method: 'GET'})
+// Ajax call to thw Giphy API. Utilizing a template literal for this line 11.    
+$.ajax({url: queryURL, method: 'GET'})
     .done(function(response) {
         var results = response.data;
         for(var i=0; i < results.length; i++){
@@ -19,6 +20,7 @@ var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + superhero + "&api_key=
             var rating = results[i].rating;
             var p = $('<p>').text( "Rating: " + rating);
             var superheroImage = $('<img>');
+            // Attributing the pausing and animation of the gifs pulled from the Giphy-API.
             superheroImage.attr('src', results[i].images.fixed_height_still.url);
             superheroImage.attr('data-still', results[i].images.fixed_height_still.url);
             superheroImage.attr('data-animate', results[i].images.fixed_height.url);
@@ -29,11 +31,11 @@ var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + superhero + "&api_key=
             $('#superherosView').append(superheroImage);
             }
         }
-    $('.superheroImage').on('click', function(){
 
+    $('.superheroImage').on('click', function(){
         var state = $(this).attr('data-state'); 
         console.log(state);
-        
+        // Code orchestraing the pausing and animating of the shown gifs on the HTML.
         if ( state === 'still'){
             $(this).attr('src', $(this).data('animate'));
             $(this).attr('data-state', 'animate');
@@ -54,11 +56,11 @@ for (var i = 0; i < superheros.length; i++){
     // Button tag is created here. 
     var a = $('<button>')
     a.addClass('superhero');
-    a.addClass("btn btn-success");
-    a.addClass("btn btn-primary btn-lg");
+    a.addClass("btn");
     a.attr('data-name', superheros[i]);
     a.text(superheros[i]);
     $('#buttonsView').append(a);
+    // I was attempting to find a way to insert width between my buttuns here, but I could not find a way. I was thinking of =trying to append calls to my variable superheros, but I was not sure. Not sure how.
 }
 }
 
