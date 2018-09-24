46655bec921f9ffc4674ebabf960bc5842d9cbb0
RichardVargas1 <richard.vargas95@gmail.com>
2018-09-23 19:45:11 -0500

    Overhaul.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,42 +1,78 @@
-// Event listener for any buttons shown on the page.
-$("button").on("click", function() {
-    
-var topics = $(this).attr("data-topics");
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=GTBBb6azu2SsBkQXEGs0WxNauL3D6UaS&limit=10";
-// Making my AJAX call.
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-    // After the data comes back from the GIPHY API.
-    .then(function(response) {
-        console.log(queryURL);
-        // stored arrayed results.
+// Beginning array of superheros.
+var superheros = ['Superman', 'Batman', 'Wonder Woman', 'Iron Man', 'Thor', 'Captain America', 'Aquaman', 'The Flash', 'Spider-Man', 'Black Panther'];
+
+function displaysuperheroInfo(){     
+
+$('#superherosView').empty();     
+    var superhero = $(this).attr('data-name');
+
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + superhero + "&api_key=dc6zaTOxFJmzC&limit=10";
+    $.ajax({url: queryURL, method: 'GET'})
+    .done(function(response) {
         var results = response.data;
+        for(var i=0; i < results.length; i++){
+            if (results[i].rating === "r" || results[i].rating === "pg-13")
+            {
+            }
+            else {
+            console.log(response)
+            var rating = results[i].rating;
+            var p = $('<p>').text( "Rating: " + rating);
+            var superheroImage = $('<img>');
+            superheroImage.attr('src', results[i].images.fixed_height_still.url);
+            superheroImage.attr('data-still', results[i].images.fixed_height_still.url);
+            superheroImage.attr('data-animate', results[i].images.fixed_height.url);
+            superheroImage.attr('data-state', 'still');
+            superheroImage.addClass('superheroImage');
+            
+            $('#superherosView').append(p);
+            $('#superherosView').append(superheroImage);
+            }
+        }
+    $('.superheroImage').on('click', function(){
 
-        for (var i = 0; i < results.length; i++) {
-
-            // div tag.
-            var heroDiv = $("<div>");
-
-            // Paragraph tag wil be utilized to space out gifs.
-            var p = $("<p>").text("Rating: " + results[i].rating);
-
-            // Creating and storing an image tag in this variable.
-            var heroImage = $("<img>");
-            // Setting the src attribute of the images to properties pulled from the URL.
-            heroImage.attr("src", results[i].images.fixed_height.url);
-
-            // heroDiv will be appended to the paragraph and image tabs.
-            heroDiv.append(p);
-            heroDiv.append(heroImage);
-
-            // My heroDiv will be prepended to my HTML page.
-            // Shown in the displayGifs div.
-            $("#displayGifs").prepend(heroDiv);
-          }
+        var state = $(this).attr('data-state'); 
+        console.log(state);
+        
+        if ( state === 'still'){
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        }else{
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        }
     });
-});
+        
+    });   
+}
 
-// I utilized the dynamics-elements assignment to help contine this homework assignment.
-// It helped start off with the jQuery aspect.
+function renderButtons(){ 
+    // Empties the superhero button, before adding a new one. That way, we will not have any buttons that repeat.
+$('#buttonsView').empty();
+    // Loops through superhero array.
+for (var i = 0; i < superheros.length; i++){
+    // Button tag is created here. 
+    var a = $('<button>')
+    a.addClass('superhero');
+    a.addClass("btn btn-success");
+    a.addClass("btn btn-primary btn-lg");
+    a.attr('data-name', superheros[i]);
+    a.text(superheros[i]);
+    $('#buttonsView').append(a);
+}
+}
+
+$('#addsuperhero').on('click', function(){ 
+    // This line of code gathers the text entered from the textbox.
+var superhero = $('#superhero-input').val().trim();
+    // Superhero is then pushed to the superhero array.
+superheros.push(superhero);
+
+renderButtons();
+
+return false;
+})
+
+$(document).on('click', '.superhero', displaysuperheroInfo);
+
+renderButtons();
